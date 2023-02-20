8615f6073f778d9667eb3e19b3cff02aaf34fa13
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-02-20 18:51:54 +0200

    added CustomAuthenticationProvider, had to add UserRepository and UserEntity in order to do task

---
+++ src/main/java/com/pvp/codingtournament/business/repository/model/UserEntity.java
@@ -0,0 +1,36 @@
+package com.pvp.codingtournament.business.repository.model;
+
+
+import com.pvp.codingtournament.business.enums.RoleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private RoleEnum role;
+}
