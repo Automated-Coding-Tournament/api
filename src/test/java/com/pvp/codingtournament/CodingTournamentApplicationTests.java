1ab94c86e7ee5ca832753e303f2e3e342df10407
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-02-20 18:32:40 +0200

    Initial commit

---
+++ src/test/java/com/pvp/codingtournament/CodingTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvp.codingtournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
