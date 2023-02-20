890681f1fd066dd44ca54360423f8aac7008bd8e
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-02-20 18:50:41 +0200

    added JWT token generation filter, security filter chain

---
+++ src/main/java/com/pvp/codingtournament/business/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.pvp.codingtournament.business.security;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "ABTDSFGB5EX51S58FB22ASd";
+    public static final String JWT_HEADER = "Authorization";
+}
