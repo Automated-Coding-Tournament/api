8a0fe7f54a11054e67f7d8a9eeafc1a2c9a06935
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-06-17 21:32:48 +0300

    changes

--- src/main/java/com/pvp/codingtournament/business/repository/TournamentRepository.java
+++ src/main/java/com/pvp/codingtournament/business/repository/TournamentRepository.java
@@ -2,12 +2,14 @@ package com.pvp.codingtournament.business.repository;
 
 import com.pvp.codingtournament.business.repository.model.TournamentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TournamentRepository extends JpaRepository<TournamentEntity, Long> {
+    @Modifying
     @Query(nativeQuery = true, value = "DELETE FROM user_tournament WHERE tournament_id = :tournamentId")
     void deleteAllRegistrationsByTournamentId(@Param("tournamentId") Long tournamentId);
 }
